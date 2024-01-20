a6cc8d31c40367a6964019ddee4f2bbb6d1960f7
Brett <brett_neighbors@yahoo.com>
2024-01-20 16:45:46 -0700

    added change and submit handlers

--- src/ProfileEdit.jsx
+++ src/ProfileEdit.jsx
@@ -7,15 +7,37 @@ component is first rendered.
 */
 
 function ProfileEdit() {
-    console.log("Started...")
     const [user, setUser] = useState({});
-  
+    console.log("render", user)
+
+    const changeHandler = event => {
+        setUser({ ...user, [event.target.name]: event.target.value })
+      }
+
+      const submitHandler = async (event) => {
+        event.preventDefault();
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/users/${user.id}`,
+          {
+            method: "PUT",
+            body: JSON.stringify(user),
+            headers: {
+              "Content-type": "application/json;charset=UTF-8"
+            }
+          }
+        );
+        const savedData = await response.json();
+        console.log("Saved user!", savedData);
+      };
+
     useEffect(() => {
+      console.log("useEffect")
       async function loadUsers() {
         const response = await fetch(
           "https://jsonplaceholder.typicode.com/users/1"
         );
         const userFromAPI = await response.json();
+        console.log("setUser", userFromAPI)
         setUser(userFromAPI);
       }
       loadUsers();
@@ -29,14 +51,23 @@ function ProfileEdit() {
             <label htmlFor="username">User Name:</label>
             <input
               id="username"
-              name="username"
+              name="wrongusername"
               type="text"
+              required={true}
               value={user.username}
+              onChange={changeHandler}
             />
           </div>
           <div>
             <label htmlFor="email">Email:</label>
-            <input id="email" name="email" type="email" value={user.email} />
+            <input
+              id="email"
+              name="email"
+              type="email"
+              required={true}
+              value={user.email}
+              onChange={changeHandler}
+            />
           </div>
         </form>
       );
