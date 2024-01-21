7a98ade79d6230fcdada8628238fb13515b61568
Brett <brett_neighbors@yahoo.com>
2024-01-20 17:01:55 -0700

    Full end of lesson code

--- src/ProfileEdit.jsx
+++ src/ProfileEdit.jsx
@@ -1,89 +1,76 @@
-import React, { useState, useEffect } from "react";
-
-/* 
-In this section, you will create a component to edit a user profile. 
-To do this, you will make an API call to fetch a user profile once, when the 
-component is first rendered.
-*/
-
 function ProfileEdit() {
-    const [user, setUser] = useState({});
-    console.log("render", user)
+  const [user, setUser] = useState({});
 
-    const changeHandler = event => {
-        setUser({ ...user, [event.target.name]: event.target.value })
-      }
-
-      const submitHandler = async (event) => {
-        event.preventDefault();
-        const response = await fetch(
-          `https://jsonplaceholder.typicode.com/users/${user.id}`,
-          {
-            method: "PUT",
-            body: JSON.stringify(user),
-            headers: {
-              "Content-type": "application/json;charset=UTF-8"
-            }
-          }
-        );
-
-        const savedData = await response.json();
-        console.log("Saved user!", savedData);
-      };
-
-    useEffect(() => {
-      console.log("useEffect")
-      async function loadUsers() {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/users/1"
-        );
-        const userFromAPI = await response.json();
-        console.log("setUser", userFromAPI)
-        setUser(userFromAPI);
-      }
-      loadUsers();
-    }, []); // Passing [] so that it only runs the effect once
-
-    // Second useEffect hook for seperate purpose
-    // This hook updates title of document with user's username if it exists
-    useEffect(() => {
-      if (user.username) {
-        document.title = `${user.username} : Edit Profile`
-      } else {
-        document.title = "Edit Profile"
-      }
-    }, [user])
-  
-    if (user.id) {
-      // `user.id` is truthy after the API call returns
-      return (
-        <form name="profileEdit">
-          <div>
-            <label htmlFor="username">User Name:</label>
-            <input
-              id="username"
-              name="wrongusername"
-              type="text"
-              required={true}
-              value={user.username}
-              onChange={changeHandler}
-            />
-          </div>
-          <div>
-            <label htmlFor="email">Email:</label>
-            <input
-              id="email"
-              name="email"
-              type="email"
-              required={true}
-              value={user.email}
-              onChange={changeHandler}
-            />
-          </div>
-        </form>
+  console.log("render", user);
+  useEffect(() => {
+    console.log("useEffect");
+    async function loadUsers() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users/1"
       );
+      const userFromAPI = await response.json();
+      console.log("setUser", userFromAPI);
+      setUser(userFromAPI);
     }
-    return "Loading...";
-  }
+    loadUsers();
+  }, []); // Passing [] so that it only runs the effect once
 
-  export default ProfileEdit;
+  useEffect(() => {
+    if (user.username) {
+      document.title = `${user.username} : Edit Profile`;
+    } else {
+      document.title = "Edit Profile";
+    }
+  }, [user]); // Rerun this effect when the user changes
+
+  const changeHandler = (event) => {
+    setUser({ ...user, [event.target.name]: event.target.value });
+  };
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${user.id}`,
+      {
+        method: "PUT",
+        body: JSON.stringify(user),
+        headers: {
+          "Content-type": "application/json;charset=UTF-8"
+        }
+      }
+    );
+    const savedData = await response.json();
+    console.log("Saved user!", savedData);
+  };
+
+  if (user.id) {
+    return (
+      <form name="profileEdit" onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="username">User Name:</label>
+          <input
+            id="username"
+            name="username"
+            type="text"
+            required={true}
+            value={user.username}
+            onChange={changeHandler}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            required={true}
+            value={user.email}
+            onChange={changeHandler}
+          />
+        </div>
+        <button type="submit">Save</button>
+      </form>
+    );
+  }
+  return "Loading...";
+}
